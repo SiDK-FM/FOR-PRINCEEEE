e9670b95bd3e186bbecb8f24f489225789a8a0bc
Anshuman <anshumanprasadmahanta@gmail.com>
2023-05-29 06:19:06 +0530

    Fixed

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SiDK-FM/FOR-NISHANTH.git /FOR-NISHANTH
+  git clone https://github.com/SiDK-FM/FOR-NISHANTH FOR-NISHANTH
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FOR-NISHANTH
+  git clone $UPSTREAM_REPO FOR-NISHANTH
 fi
-cd /Elsa
+cd FOR-NISHANTH
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
